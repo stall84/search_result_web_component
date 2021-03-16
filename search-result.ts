c04c406fc20f4f4d7ec59e1c53f469c0af273899
Michael <stall84@gmail.com>
2021-03-16 10:28:40 -0400

    Initial commit

---
+++ search-result.ts
@@ -0,0 +1,44 @@
+
+
+const template = document.createElement('template') as HTMLTemplateElement;
+
+template.innerHTML = `
+    <style>
+        div {
+            margin-top: 20px;
+            color: rgb(248,210,191);
+        }
+    </style>
+    <div>
+        <p>The Google search result of your name is <a target="_blank" rel="noopener">here!</a></p>
+    </div>
+`;
+
+class SearchResult extends HTMLElement {
+
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot!.appendChild(template.content.cloneNode(true));
+        this.shadowRoot!.querySelector('a')!.href = '';
+
+
+    }
+
+    static get observedAttributes() {
+        return ['name-attribute'];
+    }
+
+    attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+        if (name == 'name-attribute') {
+            this.shadowRoot!.querySelector(
+                'a'
+            )!.href = `https://www.google.com/search?q=${newValue}`;
+        }
+    }
+
+
+}
+
+window.customElements.define('search-result', SearchResult);
